a073b044783c938be19c68b59fdf080e9098fcc7
CamilleKerherve <camille.kerherve@etud.univ-jfc.fr>
2024-10-02 09:04:18 +0200

    Synchronized

--- src/main/java/lavage/PileDAssiettes.java
+++ src/main/java/lavage/PileDAssiettes.java
@@ -5,31 +5,35 @@ import java.util.List;
 
 class PileDAssiettes {
 
-	private final static int MAX = 10;
-	private final List<Assiette> myList = new LinkedList<>();
+    private final static int MAX = 10;
+    private final List<Assiette> myList = new LinkedList<>();
 
-	private boolean isEmpty() {
-		return myList.isEmpty();
-	}
+    private boolean isEmpty() {
+        return myList.isEmpty();
+    }
 
-	private boolean isFull() {
-		return (myList.size() >= MAX);
-	}
+    private boolean isFull() {
+        return (myList.size() >= MAX);
+    }
 
-	public void push(Assiette assiette) throws InterruptedException {
-		myList.add(assiette);
-		System.out.printf("la pile contient %d assiettes%n", myList.size());
-	}
+    synchronized public void push(Assiette assiette) throws InterruptedException {
+        while (this.isFull()) {
+            wait();
+        }
+        myList.add(assiette);
+        System.out.printf("la pile contient %d assiettes%n", myList.size());
+        notifyAll();
+    }
 
-	synchronized public Assiette pop() throws InterruptedException {
-		// Attendre que la pile ne soit pas vide
-		while (isEmpty()) {
-			wait();
-		}
-		//assert !isEmpty(); // On est sur que la pile n'est pas vide
-		Assiette result = myList.remove(myList.size() - 1);
-		System.out.printf("la pile contient %d assiettes%n", myList.size());
-		notifyAll(); // Notifier que la pile n'est plus pleine
-		return result;
-	}
+    synchronized public Assiette pop() throws InterruptedException {
+        // Attendre que la pile ne soit pas vide
+        while (isEmpty()) {
+            wait();
+        }
+        //assert !isEmpty(); // On est sur que la pile n'est pas vide
+        Assiette result = myList.remove(myList.size() - 1);
+        System.out.printf("la pile contient %d assiettes%n", myList.size());
+        notifyAll(); // Notifier que la pile n'est plus pleine
+        return result;
+    }
 }
